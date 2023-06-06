5310eb7a463a13a33f507c8bdcd3b3e3c78f4e59
Maikelwebdev <109978362+Maikelwebdev@users.noreply.github.com>
2023-06-06 11:07:40 +0300

    css fix

--- js/scripts.js
+++ js/scripts.js
@@ -66,6 +66,8 @@ function mostrarCoin(coinId)
       nav2Coins.textContent = '';
       let divCoin = document.createElement('div');
       divCoin.classList.add('divCoin')
+      divCoin.setAttribute('id', 'divCoin'); 
+
       
       let nameCripto = document.createElement('div');
       nameCripto.textContent = coin.name;
