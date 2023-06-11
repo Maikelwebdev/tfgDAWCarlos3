b0a417866827b2b327ea97d1c727571e81c401d1
Maikelwebdev <109978362+Maikelwebdev@users.noreply.github.com>
2023-06-11 22:10:21 +0300

    1

--- js/discover.js
+++ js/discover.js
@@ -68,6 +68,8 @@ let accounts = [];
 let botonDonacion = document.getElementById('bDonacion');
 let direccionDestino = '0x834999AC875E16EB769E3726F4c8884aDDCc4f63'; // dirección de billetera a la que se enviará la donación
 let montoEnviar = '0.0001';
+
+
 // Send Ethereum to an address
 botonDonacion.addEventListener('click', () => {
     ethereum
@@ -78,7 +80,7 @@ botonDonacion.addEventListener('click', () => {
             from: accounts[0], // The user's active address.
             to: '0x933Dcf0923B6F1bED3ae35dD7523D058b9325417', //  dirección de billetera a la que se enviará la donación
             value: '0x29a2241af62c0000', //  representación en Wei de 0.0001 ETH
-            gasPrice: '0x09184e72a000', // Customizable by the user during MetaMask confirmation.
+            gasPrice: '0x09184e72a000', // 15 Gwei 
             gas: '0x2710', // Customizable by the user during MetaMask confirmation.
           },
         ],
